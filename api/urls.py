3cf78785a98899eb4eefa03d5f5f5a9b65aff946
cole <colepolyak@gmail.com>
2021-01-31 13:07:27 -0700

    Added health check endpoint.

--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,4 +26,5 @@ urlpatterns = [
     path('api/v1/', include('restapi.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^hc/', include('health_check.urls')),
 ]
